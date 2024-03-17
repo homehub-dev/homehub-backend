c53d377009dfff6c74227e73e081f03d1d6845eb
Monish Thirukkumaran <monish.thir@gmail.com>
2024-03-17 21:59:16 +0000

    feat(server): add axum

--- homehub-server/src/main.rs
+++ homehub-server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use axum::routing;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let app = Router::new().route("/", routing::get(root));
+    homehub_models::connect_db().await;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
